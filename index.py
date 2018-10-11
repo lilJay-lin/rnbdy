582c7cd72e73815dccd652653e1f54f8464b92a8
liljay <lin_xjie@foxmail.com>
2018-10-11 21:05:17 +0800

    #update 增加文件夹格式化;增加文件名字符串替换

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from cmd import Cmd
 import json
 import os
 import sys
+import re
 
 f = open('account',encoding='utf-8',mode='r')
 data = f.read()
@@ -10,10 +11,11 @@ f.closed
 user = data.split('\n')
 pcs = PCS(user[0], user[1])
 
+help = '请输入命令：\n replace 替换的文件夹路径 替换的文件名 需要在文件名尾部添加的字符串\n format 格式化的文件夹路径'
 
 class Client(Cmd):
     prompt = 'fun>'
-    intro = '百度云盘文件名替换工具 \n请输入命令： replace 替换的文件夹路径 需要在文件名尾部添加的字符串\n输入exit退出'
+    intro = '百度云盘文件名替换工具 \n' + help
 
     def __init__(self):
         self.renamelist = []
@@ -22,14 +24,26 @@ class Client(Cmd):
     def do_replace(self, arg):
         self.renamelist = []
         args = arg.split()
-        if len(args) < 2:
-            print('参数缺失： replace \'替换的文件夹路径\' \'需要在文件名尾部添加的字符串\'')
+        if len(args) < 3:
+            print('参数缺失： replace \'替换的文件夹路径\' 替换的文件名 需要在文件名尾部添加的字符串')
         else:
             print('正在替换请稍后....')
-            self.get_rename_list(args[0], args[1])
+            self.get_rename_list(args[0], args[1], args[2])
             if len(self.renamelist) > 0:
                 pcs.rename(self.renamelist)
             print('替换完成')
+
+    def do_format(self, arg):
+        self.renamelist = []
+        args = arg.split()
+        if len(args) < 1:
+            print('参数缺失： format \'格式化文件夹路径\'')
+        else:
+            print('正在格式化....')
+            self._format_dir(args[0])
+            if len(self.renamelist) > 0:
+                pcs.rename(self.renamelist)
+            print('格式化完成')
 
     def do_exit(self, arg):
         print('Bye!')
@@ -55,22 +69,36 @@ class Client(Cmd):
     #     return Cmd.postcmd(self, stop, line)
 
     def emptyline(self):
-        print('请输入命令： replace 替换的文件夹路径 需要在文件名尾部添加的字符串')
+        print(help)
 
     def default(self, line):
-        print('请输入命令： replace 替换的文件夹路径 需要在文件名尾部添加的字符串')
+        print(help)
 
-    def get_rename_list(self, path, addStr):
+    def get_rename_list(self, path, reStr, addStr):
         response = pcs.list_files(path)
         searchlist = response.json()
         for file in searchlist.get('list'):
             if file.get('isdir') == 1:
-                self.get_rename_list(file.get('path'), addStr)
-            elif file.get('server_filename').find(addStr) == -1:
-                name = file.get('server_filename').partition('.')
-                self.renamelist.append((file.get('path'), name[0] + addStr + name[1] + name[2]))
+                self.get_rename_list(file.get('path'), reStr, addStr)
+            elif file.get('server_filename').find(addStr) == -1 and file.get('server_filename').find(reStr) != -1:
+                # name = file.get('server_filename').partition('.')
+                # self.renamelist.append((file.get('path'), name[0] + addStr + name[1] + name[2]))
+                name = file.get('server_filename').replace(reStr, addStr)
+                self.renamelist.append((file.get('path'), name))
                 # print(renamelist)
         return
+
+    def _format_dir(self, path):
+        response = pcs.list_files(path)
+        searchlist = response.json()
+        for file in searchlist.get('list'):
+            if file.get('isdir') == 1:
+                name = file.get('server_filename')
+                name = re.sub(r'\s+', '', name)
+                name = re.sub(r'[a-zA-Z]+', '', name)
+                name = re.sub(r'\d+', '', name)
+                print(name)
+                self.renamelist.append((file.get('path'), name))
 
 
 if __name__ == '__main__':
